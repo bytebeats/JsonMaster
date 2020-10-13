510e329b270bec0e213b6c6f669100d3ed54026c
panchen <panchen@tigerbrokers.com>
2020-10-13 21:22:15 +0800

    created TabLayout and TabView.

---
+++ src/main/java/me/bytebeats/jsonmstr/intf/OnLastTabListener.kt
@@ -0,0 +1,14 @@
+package me.bytebeats.jsonmstr.intf
+
+/**
+ * @Author bytebeats
+ * @Email <happychinapc@gmail.com>
+ * @Github https://github.com/bytebeats
+ * @Created on 2020/10/13 20:16
+ * @Version 1.0
+ * @Description TO-DO
+ */
+
+interface OnLastTabListener {
+    fun onLast()
+}
