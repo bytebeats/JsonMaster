e06b77dd9fc7398eb0a38c60f714398acec00470
Chen Pan <chen.pan@goat.com>
2025-06-26 17:32:00 +0800

    release 1.4.0.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,14 @@
+import org.jetbrains.intellij.platform.gradle.IntelliJPlatformType
+import org.jetbrains.intellij.platform.gradle.models.ProductRelease
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import java.io.FileInputStream
+import java.util.Properties
 
 plugins {
-    application
     id("java")
     id("org.jetbrains.kotlin.jvm") version "2.1.0"
-    id("org.jetbrains.intellij.platform") version "2.5.0"
+    id("org.jetbrains.intellij.platform") version "2.6.0"
 }
 
 apply(plugin = "kotlin")
@@ -43,10 +46,6 @@ dependencies {
     }
 }
 
-application {
-    mainClass = "me.bytebeats.jsonmaster.ui.JsonMasterWindow"
-}
-
 // Configure Gradle IntelliJ Plugin
 // Read more: https://plugins.jetbrains.com/docs/intellij/tools-gradle-intellij-plugin.html
 intellijPlatform {
@@ -62,6 +61,20 @@ intellijPlatform {
       v1.4.0 regular upgrade to Java 21 and Idea 2025.<br>
     """.trimIndent()
     }
+
+    pluginVerification {
+        ides {
+            ide(IntelliJPlatformType.IntellijIdeaCommunity, "2024.3.6")
+            local(file(project.localProperty("plugin.verifier.ide.location")))
+            recommended()
+            select {
+                types = listOf(IntelliJPlatformType.IntellijIdeaCommunity, IntelliJPlatformType.AndroidStudio)
+                channels = listOf(ProductRelease.Channel.RELEASE)
+                sinceBuild = "242"
+                untilBuild = "251.*"
+            }
+        }
+    }
 }
 
 tasks {
@@ -72,6 +85,10 @@ tasks {
     }
     withType<KotlinCompile> {
         compilerOptions.jvmTarget.set(JvmTarget.JVM_21)
+    }
+
+    buildSearchableOptions {
+        enabled = false
     }
 
     signPlugin {
@@ -89,18 +106,23 @@ tasks {
     }
 
     register<Copy>("MoveBuildArtifacts") {
-        dependsOn(named("distZip"))
         mustRunAfter("DeletePluginFiles")
         println("Moving Build Artifacts!")
         from(layout.buildDirectory.dir("distributions"))
-        include("Json Master-$version.zip")
+        include("JsonMaster-$version.zip")
         into("plugins")
     }
 
     register<Delete>("DeletePluginFiles") {
         delete(files("plugins"))
     }
-    named("build") {
+    named("signPlugin") {
         finalizedBy("MoveBuildArtifacts")
     }
+}
+
+fun Project.localProperty(key: String, from: String = "local.properties"): String {
+    val properties = Properties()
+    properties.load(FileInputStream(rootProject.file(from)))
+    return properties.getProperty(key)
 }
